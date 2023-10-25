6339610c164a187c21821812be5b6e9a3561f01d
unknown <rajputhusama@gmail.com>
2023-10-25 12:12:20 -0700

    admin css set

---
+++ resources/js/Layouts/AdminLayout.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue';
+import Navbar from "@/components/Admin/Navbar.vue";
+import { Link } from '@inertiajs/vue3';
+import('../../css/admin/style.css');
+import('../../css/admin/components.css');
+import('../')
+
+
+</script>
+
+
+
+<template>
+
+
+
+    <Navbar/>
+</template>
